95a21809411f0d3b5f5955739deb6d19faa2bc70
kallistahalim <kallistahalim@gmail.com>
2020-10-29 02:01:41 +0700

    add will

--- case-sato/will.js
+++ case-sato/will.js
@@ -5,7 +5,11 @@ $("#submit").on("click", function() {
     console.log(compareWillId);
     $("#will-id").empty();
     if (compareWillId === "V8SI1EIN") {
-        $("#will-png").html("<img id='will-letter' src=../assets/prize/AF-100.png>");
+        $("#will-png1").html("<img id='will-letter' src=../assets/sato-last-will-signed/1.png>");
+        $("#will-png2").html("<img id='will-letter' src=../assets/sato-last-will-signed/2.png>");
+        $("#will-png3").html("<img id='will-letter' src=../assets/sato-last-will-signed/3.png>");
+        $("#will-png4").html("<img id='will-letter' src=../assets/sato-last-will-signed/4.png>");
+        $("#will-png5").html("<img id='will-letter' src=../assets/sato-last-will-signed/5.png>");
     } else {
         $("#will-png").html("you're not worthy to read my will");
     }
