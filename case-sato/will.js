97aa9c6bb350ffe3ef3565295a4cfd6ed7853171
kallistahalim <kallistahalim@gmail.com>
2020-11-09 20:59:59 +0700

    fix bug

--- case-sato/will.js
+++ case-sato/will.js
@@ -4,7 +4,7 @@ $("#submit").on("click", function() {
     console.log(willId);
     console.log(compareWillId);
     $("#will-id").empty();
-    if (compareWillId === "V8SI1EIN") {
+    if (compareWillId === "V8LI1EIN") {
         $("#will-png1").html("<img id='will-letter' src=../assets/sato-last-will-signed/1.png>");
         $("#will-png2").html("<img id='will-letter' src=../assets/sato-last-will-signed/2.png>");
         $("#will-png3").html("<img id='will-letter' src=../assets/sato-last-will-signed/3.png>");
