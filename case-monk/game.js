0959e46ea336fdacd8c5840c4e0352d15e9e3e89
kallistahalim <kallistahalim@gmail.com>
2021-02-01 02:57:47 +0700

    works until timer

--- case-monk/game.js
+++ case-monk/game.js
@@ -19,17 +19,19 @@ firebase.database().ref().once("value").then(function (snapshot) {
 
     $("#submit").on("click", function () {
         gameID = $("#game-id-input").val();
+        console.log(gameID);
+        console.log(snapshot.val().user.order[gameID]);
         $("#game-id").empty();
         if (snapshot.val().user.order[gameID] == undefined) {
             $("image-buttons").html("I am sorry we could not verify your ID Number. Please contact +62 81 1952 6700");
         } else {
-            AorB = snapshot.val().order[gameID].AorB;
+            AorB = snapshot.val().user.order[gameID].AorB;
             if (AorB === "A") {
                 startGameA();
             } else {
                 startGameB();
             }
-            
+
         }
 
     })
@@ -45,41 +47,54 @@ var playerA = [iconOptions[3], iconOptions[1], iconOptions[2], iconOptions[8], i
 //images shown for player B
 var playerB = [iconOptions[0], iconOptions[7], iconOptions[9], iconOptions[6], iconOptions[3], iconOptions[7], iconOptions[8], iconOptions[1], iconOptions[5], iconOptions[4]];
 
-//print out image buttons
-for (var i = 0; i < iconOptions.length; i++) {
-    var button = $("<button>");
-    button.addClass("game-button");
-    button.attr("data-let", (i));
-    button.html("<img src =" + iconOptions[i] + ".png>");
-    $("#image-buttons").append(button);
-}
+
 
 //render game
+var a = 0;
 
-var i = 0; 
-
+//how playerA starts
 function startGameA() {
-    if (i >= playerA.length) {
+    console.log(a);
+
+    //print out image buttons
+    for (var i = 0; i < iconOptions.length; i++) {
+        var button = $("<button>");
+        button.addClass("game-button");
+        button.attr("data-let", (i));
+        button.html("<img src =" + iconOptions[i] + ".png>");
+        $("#image-buttons").append(button);
+    }
+
+    //game is done
+    if (a >= playerA.length) {
         $("#top-image").empty();
         $("#image-buttons").html("Open __________");
+        return;
+
+        //game starts and continue
+    } else {
+
+        //emptyDiv
+        function emptyDiv() {
+            $("#top-image").empty();
+        }
+
+        //player A
+        function playerAImage() {
+            for (var j = 0; j < playerA.length; j++) {
+                $("#top-image").html("<img class = 'game-image' src=" + playerA[j] + ".png>");
+            }
+        }
+
+        $("#top-image").ready(function () {
+            setTimeout(playerAImage, 3000);
+            if (setTimeout(playerAImage, 3000)) {
+                setTimeout(emptyDiv, 3000);
+            }
+
+        })
+        
     }
 
-}
 
-//emptyDiv
-function emptyDiv() {
-    $("#top-image").empty();
 }
-
-//player A
-function playerAImage() {
-    for (var j = 0; j < playerA.length; j++) {
-        $("#top-image").html("<img class = 'game-image' src=" + playerA[j] + ".png>");
-    }
-}
-
-$("#top-image").ready(function () {
-    console.log("Hi");
-    setTimeout(playerAImage, 3000);
-    // setTimeout(emptyDiv, 3000);
-})
