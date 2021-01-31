51ac442b0c1c4fbaf50212633d9c5418edf5f2ef
kallistahalim <kallistahalim@gmail.com>
2021-02-01 02:07:07 +0700

    aassd

--- case-monk/game.js
+++ case-monk/game.js
@@ -28,7 +28,6 @@ firebase.database().ref().once("value").then(function (snapshot) {
 
 })
 
-
 //images submitted
 var iconOptions = ["./icon/1", "./icon/2", "./icon/3", "./icon/4", "./icon/5", "./icon/6", "./icon/7", "./icon/8", "./icon/9", "./icon/10"];
 
@@ -45,6 +44,15 @@ for (var i = 0; i < iconOptions.length; i++) {
     button.attr("data-let", (i));
     button.html("<img src =" + iconOptions[i] + ".png>");
     $("#image-buttons").append(button);
+}
+
+//render game
+
+var i = 0;
+
+function startGame() {
+    if (i)
+
 }
 
 //emptyDiv
